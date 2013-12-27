32ee9731607d9b44702a38d90a726cce97573431
roderick3427 <roderickjackson3427@gmail.com>
2013-12-26 22:42:37 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RapTatts::Application.config.secret_key_base = 'c069d286433a785c86cbb0951c11f4dec1f92ad63e9ce8ef8ba123fe350ca6d234007d4da922a28baba1574997c7abcdef049db314bac47a046aa235b9c8050a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RapTatts::Application.config.session_store :cookie_store, key: '_rap_tatts_session'
